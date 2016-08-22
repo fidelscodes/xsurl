5fad33d8324db3b88f6abc57ddd8534f015cacb2
fidelscodes <fidelscodes@gmail.com>
2016-08-21 23:57:51 -0400

    create User and Link models

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,1 +1,4 @@
-
+class Link < ActiveRecord::Base
+  belongs_to :user
+  validates_presence_of :url, :short_url
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,1 +1,5 @@
-
+class User < ActiveRecord::Base
+  has_many :links
+  has_secure_password
+  validates_presence_of :username, :email, :password
+end
