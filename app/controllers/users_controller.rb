70fccab0a3f513522debc8325598c0b03c3e5318
fidelscodes <fidelscodes@gmail.com>
2016-08-22 09:16:32 -0400

    add actions for post signup and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,19 @@ class UsersController < ApplicationController
     erb :'/users/new'
   end
 
+  post '/signup' do
+    @user = User.create(params)
+    session[:user_id] = @user.id
+    erb :"/users/#{@user.id}/show" # make it redirect to /users/show
+  end
+
   get '/login' do
     erb :'/users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(params[:username])
+    session[:user_id] = @user.id
+    erb :"/users/#{@user.id}/show"
   end
 end
