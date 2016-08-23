55ee6270f52ecd3a26bef2fc5161e3d19f3cdf31
fidelscodes <fidelscodes@gmail.com>
2016-08-22 22:36:51 -0400

    fix issue with dead cancel button on login page, update link model to make links shorter

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,7 +3,7 @@ class Link < ActiveRecord::Base
   validates_presence_of :url
 
   def create_short_url
-    self.random_hex_string = SecureRandom.urlsafe_base64(5)
+    self.random_hex_string = SecureRandom.urlsafe_base64(3)
     self.short_url = 'http://localhost:9393' + "/#{random_hex_string}"
     self.save
   end
